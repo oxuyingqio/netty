4fc85d2715965b24ef7906c7694e05ffb32a3e28
oxuyingqio <oxuyingqio@qq.com>
2016-07-15 17:34:58 +0800

    x

--- src/main/java/cn/xuyingqi/netty/study/Server.java
+++ src/main/java/cn/xuyingqi/netty/study/Server.java
@@ -1,14 +1,56 @@
 package cn.xuyingqi.netty.study;
 
+import io.netty.bootstrap.ServerBootstrap;
+import io.netty.channel.ChannelFuture;
+import io.netty.channel.ChannelInitializer;
+import io.netty.channel.ChannelOption;
 import io.netty.channel.EventLoopGroup;
 import io.netty.channel.nio.NioEventLoopGroup;
+import io.netty.channel.socket.SocketChannel;
+import io.netty.channel.socket.nio.NioServerSocketChannel;
+import io.netty.handler.codec.LineBasedFrameDecoder;
+import io.netty.handler.codec.string.StringDecoder;
 
 public class Server {
 
 	public void bind(int port) {
 
+		// 服务器线程组
+		// 用于接收客户端连接
 		EventLoopGroup bossGroup = new NioEventLoopGroup();
+		// 用于客户端连接读写
 		EventLoopGroup workerGroup = new NioEventLoopGroup();
 
+		ServerBootstrap b = new ServerBootstrap();
+		b.group(bossGroup, workerGroup).channel(NioServerSocketChannel.class).option(ChannelOption.SO_BACKLOG, 1024)
+				.childHandler(new ChildChannelHandler());
+
+		try {
+			// 同步绑定端口号
+			ChannelFuture f = b.bind(port).sync();
+			// 同步等待端口关闭
+			f.channel().closeFuture().sync();
+		} catch (InterruptedException e) {
+			e.printStackTrace();
+		} finally {
+			// 释放线程池资源
+			bossGroup.shutdownGracefully();
+			workerGroup.shutdownGracefully();
+		}
+	}
+
+	private class ChildChannelHandler extends ChannelInitializer<SocketChannel> {
+
+		@Override
+		protected void initChannel(SocketChannel arg0) throws Exception {
+			arg0.pipeline().addLast(new LineBasedFrameDecoder(1024));
+			arg0.pipeline().addLast(new StringDecoder());
+			arg0.pipeline().addLast(new ServerHandler());
+		}
+	}
+
+	public static void main(String[] args) {
+
+		new Server().bind(9999);
 	}
 }

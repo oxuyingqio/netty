58dd585813ee398a817366aecc4d30a01342550c
oxuyingqio <oxuyingqio@qq.com>
2016-07-19 16:20:56 +0800

    X

---
+++ src/main/java/cn/xuyingqi/netty/server/core/Constant.java
@@ -0,0 +1,15 @@
+package cn.xuyingqi.netty.server.core;
+
+/**
+ * 系统常量
+ * 
+ * @author XuYQ
+ *
+ */
+public final class Constant {
+
+	/**
+	 * 服务器配置文件
+	 */
+	public static final String SERVER_CONFIG_FILE = "server.xml";
+}

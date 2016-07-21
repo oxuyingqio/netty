6dec94d4bfd9b927ab4c8daeffc503d46d55665d
oxuyingqio <oxuyingqio@qq.com>
2016-07-21 16:43:29 +0800

    X

--- src/main/java/cn/xuyingqi/netty/server/container/ServletContainer.java
+++ src/main/java/cn/xuyingqi/netty/server/container/ServletContainer.java
@@ -1,5 +1,14 @@
 package cn.xuyingqi.netty.server.container;
 
+import java.util.List;
+import java.util.Map;
+
+import cn.xuyingqi.netty.server.core.ServerAppXml;
+import cn.xuyingqi.netty.server.core.ServerAppXml.ServletConfig;
+import cn.xuyingqi.netty.server.servlet.DefaultServletConfig;
+import cn.xuyingqi.netty.server.servlet.Servlet;
+import cn.xuyingqi.util.util.MapFactory;
+
 /**
  * Servlet容器
  * 
@@ -12,13 +21,76 @@ public final class ServletContainer {
 	 * Servlet容器
 	 */
 	private static ServletContainer servletContainer;
-	
-	
-	
+
+	/**
+	 * Servlet集合
+	 */
+	private static Map<String, Servlet> servlets = MapFactory.newInstance();
+
 	/**
 	 * 私有构造方法
 	 */
 	private ServletContainer() {
 
+		// 获取Servlet配置集合
+		List<ServletConfig> servletConfigs = ServerAppXml.getInstance().getServletConfigs();
+
+		try {
+
+			// 遍历Servlet配置集合
+			for (int i = 0, length = servletConfigs.size(); i < length; i++) {
+
+				// Servlet配置
+				ServletConfig servletConfig = servletConfigs.get(i);
+
+				// 实例化Servlet
+				Servlet servlet = (Servlet) this.getClass().getClassLoader().loadClass(servletConfig.getClassName())
+						.newInstance();
+				// 初始化Servlet
+				servlet.init(new DefaultServletConfig((cn.xuyingqi.netty.server.servlet.ServletContext) this.getClass()
+						.getClassLoader().loadClass(servletConfig.getContextConfig().getClassName()).newInstance()));
+				// 添加Servlet
+				this.addServlet(servletConfig.getName(), servlet);
+			}
+		} catch (InstantiationException | IllegalAccessException | ClassNotFoundException e) {
+			e.printStackTrace();
+		}
+	}
+
+	/**
+	 * 获取Servlet容器实例
+	 * 
+	 * @return
+	 */
+	public static final ServletContainer getInstance() {
+
+		if (servletContainer == null) {
+			servletContainer = new ServletContainer();
+		}
+
+		return servletContainer;
+	}
+
+	/**
+	 * 添加Servlet
+	 * 
+	 * @param name
+	 *            Servlet名称
+	 * @param servlet
+	 *            Servlet
+	 */
+	private void addServlet(String name, Servlet servlet) {
+		servlets.put(name, servlet);
+	}
+
+	/**
+	 * 获取Servlet
+	 * 
+	 * @param name
+	 *            Servlet名称
+	 * @return
+	 */
+	public Servlet getServlet(String name) {
+		return servlets.get(name);
 	}
 }

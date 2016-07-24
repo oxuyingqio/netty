0eafbe1bff33a4583aecbe4b143c138f7f794e4b
XuYQ <XuYQ@XuYQ-PC>
2016-07-24 23:11:03 +0800

    x

--- src/main/java/cn/xuyingqi/netty/server/servlet/DefaultServletConfig.java
+++ src/main/java/cn/xuyingqi/netty/server/servlet/DefaultServletConfig.java
@@ -1,7 +1,11 @@
 package cn.xuyingqi.netty.server.servlet;
 
+import java.util.Map;
+import java.util.Set;
+
 import cn.xuyingqi.net.servlet.ServletContext;
 import cn.xuyingqi.net.servlet.impl.AbstractServletConfig;
+import cn.xuyingqi.util.util.MapFactory;
 
 /**
  * 默认的Servlet配置
@@ -12,19 +16,53 @@ import cn.xuyingqi.net.servlet.impl.AbstractServletConfig;
 public class DefaultServletConfig extends AbstractServletConfig {
 
 	/**
-	 * 默认的Servlet配置
+	 * 初始化参数
 	 */
-	public DefaultServletConfig() {
-		super(new DefaultServletContext());
-	}
+	private Map<String, Object> initParamter = MapFactory.newInstance();
+
+	/**
+	 * Servlet名称
+	 */
+	private String servletName;
+
+	/**
+	 * Servlet上下文
+	 */
+	private ServletContext context;
 
 	/**
 	 * 默认的Servlet配置
 	 * 
-	 * @param servletContext
-	 *            Servlet上下文
+	 * @param initParamter
+	 *            初始化参数
 	 */
-	public DefaultServletConfig(ServletContext servletContext) {
-		super(servletContext);
+	public DefaultServletConfig(String servletName, Map<String, Object> initParamter) {
+
+		this.servletName = servletName;
+		this.initParamter = initParamter;
+	}
+
+	@Override
+	public Set<String> getInitParameterNames() {
+
+		return initParamter.keySet();
+	}
+
+	@Override
+	public Object getInitParameter(String name) {
+
+		return initParamter.get(name);
+	}
+
+	@Override
+	public ServletContext getServletContext() {
+
+		return null;
+	}
+
+	@Override
+	public String getServletName() {
+
+		return this.servletName;
 	}
 }

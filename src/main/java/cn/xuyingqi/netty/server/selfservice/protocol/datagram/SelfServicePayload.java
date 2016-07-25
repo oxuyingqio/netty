aa81e1e3aea7ffed74236f0842e911332982603a
oxuyingqio <oxuyingqio@qq.com>
2016-07-25 18:47:16 +0800

    X

--- src/main/java/cn/xuyingqi/netty/server/selfservice/protocol/datagram/SelfServicePayload.java
+++ src/main/java/cn/xuyingqi/netty/server/selfservice/protocol/datagram/SelfServicePayload.java
@@ -9,12 +9,15 @@ import cn.xuyingqi.netty.server.connector.protocol.datagram.ServerPayload;
 import cn.xuyingqi.netty.server.selfservice.protocol.datagram.constant.ICCardRechargeRequestConstant;
 import cn.xuyingqi.netty.server.selfservice.protocol.datagram.constant.PaymentRequestConstant;
 import cn.xuyingqi.netty.server.selfservice.protocol.datagram.constant.QueryICCardInfoRequestConstant;
+import cn.xuyingqi.netty.server.selfservice.protocol.datagram.constant.ReportWriteICCardRequestConstant;
+import cn.xuyingqi.netty.server.selfservice.protocol.datagram.constant.RollbackPaymentRequestConstant;
 import cn.xuyingqi.netty.server.selfservice.protocol.datagram.constant.TerminalAuthenticationRequestConstant;
 import cn.xuyingqi.netty.server.selfservice.protocol.datagram.prototype.Payload;
 import cn.xuyingqi.netty.server.selfservice.protocol.datagram.prototype.impl.request.ICCardRechargeRequest;
 import cn.xuyingqi.netty.server.selfservice.protocol.datagram.prototype.impl.request.PaymentRequest;
 import cn.xuyingqi.netty.server.selfservice.protocol.datagram.prototype.impl.request.QueryICCardInfoRequest;
 import cn.xuyingqi.netty.server.selfservice.protocol.datagram.prototype.impl.request.ReportWriteICCardRequest;
+import cn.xuyingqi.netty.server.selfservice.protocol.datagram.prototype.impl.request.RollbackPaymentRequest;
 import cn.xuyingqi.netty.server.selfservice.protocol.datagram.prototype.impl.request.TerminalAuthenticationRequest;
 import cn.xuyingqi.netty.server.selfservice.protocol.datagram.prototype.impl.request.TerminalTimingRequest;
 import cn.xuyingqi.util.util.ByteUtils;
@@ -52,68 +55,160 @@ public class SelfServicePayload implements ServerPayload {
 
 		try {
 			if (payload instanceof TerminalAuthenticationRequest) {
-				map.put(TerminalAuthenticationRequestConstant.TERMINAL_RANDOM,
-						ByteUtils.byteArray2Long(((TerminalAuthenticationRequest) payload).getTerminalRandom()));
-				map.put(TerminalAuthenticationRequestConstant.TERMINAL_TIME_STAMP, SDF.parse(ByteUtils.byteArray2String(
-						ByteUtils.bcd2ByteArray(((TerminalAuthenticationRequest) payload).getTerminalTimeStamp()))));
+				initTerminalAuthenticationRequest((TerminalAuthenticationRequest) payload);
 			} else if (payload instanceof TerminalTimingRequest) {
-
+				initTerminalTimingRequest((TerminalTimingRequest) payload);
 			} else if (payload instanceof QueryICCardInfoRequest) {
-				map.put(QueryICCardInfoRequestConstant.ICCARD_TYPE,
-						ByteUtils.byteArray2Short(((QueryICCardInfoRequest) payload).getIcCardType()));
-				map.put(QueryICCardInfoRequestConstant.ICCARD_PARTITION,
-						ByteUtils.byteArray2Int(((QueryICCardInfoRequest) payload).getIcCardPartition()));
-				map.put(QueryICCardInfoRequestConstant.ICCARD_DATA_LENGTH,
-						ByteUtils.byteArray2Int(((QueryICCardInfoRequest) payload).getIcCardDataLength()));
-				map.put(QueryICCardInfoRequestConstant.ICCARD_DATA, ((QueryICCardInfoRequest) payload).getIcCardData());
+				initQueryICCardInfoRequest((QueryICCardInfoRequest) payload);
 			} else if (payload instanceof ICCardRechargeRequest) {
-				map.put(ICCardRechargeRequestConstant.ICCARD_TYPE,
-						ByteUtils.byteArray2Short(((ICCardRechargeRequest) payload).getIcCardType()));
-				map.put(ICCardRechargeRequestConstant.ICCARD_PARTITION,
-						ByteUtils.byteArray2Int(((ICCardRechargeRequest) payload).getIcCardPartition()));
-				map.put(ICCardRechargeRequestConstant.ICCARD_DATA_LENGTH,
-						ByteUtils.byteArray2Int(((ICCardRechargeRequest) payload).getIcCardDataLength()));
-				map.put(ICCardRechargeRequestConstant.ICCARD_DATA, ((ICCardRechargeRequest) payload).getIcCardData());
-				map.put(ICCardRechargeRequestConstant.BUY_GAS,
-						ByteUtils.byteArray2Long(((ICCardRechargeRequest) payload).getBuyGas()));
-				map.put(ICCardRechargeRequestConstant.ENTRUST_UNIT_NO,
-						new String(((ICCardRechargeRequest) payload).getEntrustUnitNo(), CHARSET));
-				map.put(ICCardRechargeRequestConstant.TERMINAL_BIZ_SN,
-						ByteUtils.byteArray2Long(((ICCardRechargeRequest) payload).getTerminalBizSN()));
+				initICCardRechargeRequest((ICCardRechargeRequest) payload);
 			} else if (payload instanceof PaymentRequest) {
-				map.put(PaymentRequestConstant.ICCARD_TYPE,
-						ByteUtils.byteArray2Short(((PaymentRequest) payload).getIcCardType()));
-				map.put(PaymentRequestConstant.ICCARD_CONTROLLER,
-						new String(((PaymentRequest) payload).getIcCardController(), CHARSET));
-				map.put(PaymentRequestConstant.ICCARD_PARTITION,
-						ByteUtils.byteArray2Int(((PaymentRequest) payload).getIcCardPartition()));
-				map.put(PaymentRequestConstant.ICCARD_DATA_LENGTH,
-						ByteUtils.byteArray2Int(((PaymentRequest) payload).getIcCardDataLength()));
-				map.put(PaymentRequestConstant.ICCARD_DATA, ((PaymentRequest) payload).getIcCardData());
-				map.put(PaymentRequestConstant.ICCARD_NO,
-						new String(((PaymentRequest) payload).getIcCardNo(), CHARSET));
-				map.put(PaymentRequestConstant.USER_CODE,
-						new String(((PaymentRequest) payload).getUserCode(), CHARSET));
-				map.put(PaymentRequestConstant.USER_NAME,
-						new String(((PaymentRequest) payload).getUserName(), CHARSET));
-				map.put(PaymentRequestConstant.BUY_TIME,
-						ByteUtils.byteArray2Int(((PaymentRequest) payload).getBuyTime()));
-				map.put(PaymentRequestConstant.BUY_GAS,
-						ByteUtils.byteArray2Long(((PaymentRequest) payload).getBuyGas()));
-				map.put(PaymentRequestConstant.RECEIVABLE,
-						ByteUtils.byteArray2Long(((PaymentRequest) payload).getReceivable()));
-				map.put(PaymentRequestConstant.ENTRUST_UNIT_NO,
-						new String(((PaymentRequest) payload).getEntrustUnitNo(), CHARSET));
-				map.put(PaymentRequestConstant.TERMINAL_BIZ_SN,
-						ByteUtils.byteArray2Long(((PaymentRequest) payload).getTerminalBizSN()));
+				initPaymentRequest((PaymentRequest) payload);
 			} else if (payload instanceof ReportWriteICCardRequest) {
-
+				initReportWriteICCardRequest((ReportWriteICCardRequest) payload);
+			} else if (payload instanceof RollbackPaymentRequest) {
+				initRollbackPaymentRequest((RollbackPaymentRequest) payload);
 			}
 		} catch (ParseException e) {
 			e.printStackTrace();
 		} catch (UnsupportedEncodingException e) {
 			e.printStackTrace();
 		}
+	}
+
+	/**
+	 * 终端认证请求
+	 * 
+	 * @param request
+	 * @throws ParseException
+	 */
+	private void initTerminalAuthenticationRequest(TerminalAuthenticationRequest request) throws ParseException {
+		map.put(TerminalAuthenticationRequestConstant.TERMINAL_RANDOM,
+				ByteUtils.byteArray2Long(request.getTerminalRandom()));
+		map.put(TerminalAuthenticationRequestConstant.TERMINAL_TIME_STAMP,
+				SDF.parse(ByteUtils.byteArray2String(ByteUtils.bcd2ByteArray(request.getTerminalTimeStamp()))));
+	}
+
+	/**
+	 * 终端校时请求
+	 * 
+	 * @param request
+	 */
+	private void initTerminalTimingRequest(TerminalTimingRequest request) {
+
+	}
+
+	/**
+	 * IC卡信息查询请求
+	 * 
+	 * @param request
+	 */
+	private void initQueryICCardInfoRequest(QueryICCardInfoRequest request) {
+		map.put(QueryICCardInfoRequestConstant.ICCARD_TYPE, ByteUtils.byteArray2Short(request.getIcCardType()));
+		map.put(QueryICCardInfoRequestConstant.ICCARD_PARTITION, ByteUtils.byteArray2Int(request.getIcCardPartition()));
+		map.put(QueryICCardInfoRequestConstant.ICCARD_DATA_LENGTH,
+				ByteUtils.byteArray2Int(request.getIcCardDataLength()));
+		map.put(QueryICCardInfoRequestConstant.ICCARD_DATA, request.getIcCardData());
+	}
+
+	/**
+	 * 充值计费请求
+	 * 
+	 * @param request
+	 * @throws UnsupportedEncodingException
+	 */
+	private void initICCardRechargeRequest(ICCardRechargeRequest request) throws UnsupportedEncodingException {
+		map.put(ICCardRechargeRequestConstant.ICCARD_TYPE, ByteUtils.byteArray2Short(request.getIcCardType()));
+		map.put(ICCardRechargeRequestConstant.ICCARD_PARTITION, ByteUtils.byteArray2Int(request.getIcCardPartition()));
+		map.put(ICCardRechargeRequestConstant.ICCARD_DATA_LENGTH,
+				ByteUtils.byteArray2Int(request.getIcCardDataLength()));
+		map.put(ICCardRechargeRequestConstant.ICCARD_DATA, request.getIcCardData());
+		map.put(ICCardRechargeRequestConstant.BUY_GAS, ByteUtils.byteArray2Long(request.getBuyGas()));
+		map.put(ICCardRechargeRequestConstant.ENTRUST_UNIT_NO, new String(request.getEntrustUnitNo(), CHARSET));
+		map.put(ICCardRechargeRequestConstant.TERMINAL_BIZ_SN, ByteUtils.byteArray2Long(request.getTerminalBizSN()));
+	}
+
+	/**
+	 * 扣款请求
+	 * 
+	 * @param request
+	 * @throws UnsupportedEncodingException
+	 */
+	private void initPaymentRequest(PaymentRequest request) throws UnsupportedEncodingException {
+		map.put(PaymentRequestConstant.ICCARD_TYPE, ByteUtils.byteArray2Short(request.getIcCardType()));
+		map.put(PaymentRequestConstant.ICCARD_CONTROLLER, new String(request.getIcCardController(), CHARSET));
+		map.put(PaymentRequestConstant.ICCARD_PARTITION, ByteUtils.byteArray2Int(request.getIcCardPartition()));
+		map.put(PaymentRequestConstant.ICCARD_DATA_LENGTH, ByteUtils.byteArray2Int(request.getIcCardDataLength()));
+		map.put(PaymentRequestConstant.ICCARD_DATA, request.getIcCardData());
+		map.put(PaymentRequestConstant.ICCARD_NO, new String(request.getIcCardNo(), CHARSET));
+		map.put(PaymentRequestConstant.USER_CODE, new String(request.getUserCode(), CHARSET));
+		map.put(PaymentRequestConstant.USER_NAME, new String(request.getUserName(), CHARSET));
+		map.put(PaymentRequestConstant.BUY_TIME, ByteUtils.byteArray2Int(request.getBuyTime()));
+		map.put(PaymentRequestConstant.BUY_GAS, ByteUtils.byteArray2Long(request.getBuyGas()));
+		map.put(PaymentRequestConstant.RECEIVABLE, ByteUtils.byteArray2Long(request.getReceivable()));
+		map.put(PaymentRequestConstant.ENTRUST_UNIT_NO, new String(request.getEntrustUnitNo(), CHARSET));
+		map.put(PaymentRequestConstant.TERMINAL_BIZ_SN, ByteUtils.byteArray2Long(request.getTerminalBizSN()));
+	}
+
+	/**
+	 * 上报写卡数据请求
+	 * 
+	 * @param request
+	 * @throws UnsupportedEncodingException
+	 * @throws ParseException
+	 */
+	private void initReportWriteICCardRequest(ReportWriteICCardRequest request)
+			throws UnsupportedEncodingException, ParseException {
+		map.put(ReportWriteICCardRequestConstant.ICCARD_TYPE, ByteUtils.byteArray2Short(request.getIcCardType()));
+		map.put(ReportWriteICCardRequestConstant.ICCARD_CONTROLLER, new String(request.getIcCardController(), CHARSET));
+		map.put(ReportWriteICCardRequestConstant.ICCARD_NO, new String(request.getIcCardNo(), CHARSET));
+		map.put(ReportWriteICCardRequestConstant.USER_CODE, new String(request.getUserCode(), CHARSET));
+		map.put(ReportWriteICCardRequestConstant.USER_NAME, new String(request.getUserName(), CHARSET));
+		map.put(ReportWriteICCardRequestConstant.BUY_TIME, ByteUtils.byteArray2Int(request.getBuyTime()));
+		map.put(ReportWriteICCardRequestConstant.BUY_GAS, ByteUtils.byteArray2Long(request.getBuyGas()));
+		map.put(ReportWriteICCardRequestConstant.BUY_COST, ByteUtils.byteArray2Long(request.getBuyCost()));
+		map.put(ReportWriteICCardRequestConstant.PRESTORE, ByteUtils.byteArray2Long(request.getPrestore()));
+		map.put(ReportWriteICCardRequestConstant.PAYMENT, ByteUtils.byteArray2Long(request.getPayment()));
+		map.put(ReportWriteICCardRequestConstant.GAS_PRICE_1, ByteUtils.byteArray2Int(request.getGasPrice1()));
+		map.put(ReportWriteICCardRequestConstant.GAS_VOLUME_1, ByteUtils.byteArray2Int(request.getGasVolume1()));
+		map.put(ReportWriteICCardRequestConstant.GAS_PRICE_2, ByteUtils.byteArray2Int(request.getGasPrice2()));
+		map.put(ReportWriteICCardRequestConstant.GAS_VOLUME_2, ByteUtils.byteArray2Int(request.getGasVolume2()));
+		map.put(ReportWriteICCardRequestConstant.GAS_PRICE_3, ByteUtils.byteArray2Int(request.getGasPrice3()));
+		map.put(ReportWriteICCardRequestConstant.GAS_VOLUME_3, ByteUtils.byteArray2Int(request.getGasVolume3()));
+		map.put(ReportWriteICCardRequestConstant.GAS_PRICE_4, ByteUtils.byteArray2Int(request.getGasPrice4()));
+		map.put(ReportWriteICCardRequestConstant.GAS_VOLUME_4, ByteUtils.byteArray2Int(request.getGasVolume4()));
+		map.put(ReportWriteICCardRequestConstant.BANK_SD, new String(request.getBankSD(), CHARSET));
+		map.put(ReportWriteICCardRequestConstant.PAYMENT_DATE,
+				SDF.parse(ByteUtils.byteArray2String(ByteUtils.bcd2ByteArray(request.getPaymentDate()))));
+		map.put(ReportWriteICCardRequestConstant.WRITE_ICCARD_STATE,
+				ByteUtils.byteArray2Short(request.getWriteICCardState()));
+		map.put(ReportWriteICCardRequestConstant.EXCEPTION_CAUSE, new String(request.getExceptionCause(), CHARSET));
+		map.put(ReportWriteICCardRequestConstant.WRITE_ICCARD_DATA_LENGTH,
+				ByteUtils.byteArray2Int(request.getWriteICCardDataLength()));
+		map.put(ReportWriteICCardRequestConstant.WRITE_ICCARD_DATA, request.getWriteICCardData());
+		map.put(ReportWriteICCardRequestConstant.ENTRUST_UNIT_NO, new String(request.getEntrustUnitNo(), CHARSET));
+		map.put(ReportWriteICCardRequestConstant.TERMINAL_BIZ_SN, ByteUtils.byteArray2Long(request.getTerminalBizSN()));
+	}
+
+	/**
+	 * 冲正请求
+	 * 
+	 * @param request
+	 * @throws UnsupportedEncodingException
+	 */
+	private void initRollbackPaymentRequest(RollbackPaymentRequest request) throws UnsupportedEncodingException {
+		map.put(RollbackPaymentRequestConstant.ICCARD_TYPE, ByteUtils.byteArray2Short(request.getIcCardType()));
+		map.put(RollbackPaymentRequestConstant.ICCARD_CONTROLLER, new String(request.getIcCardController(), CHARSET));
+		map.put(RollbackPaymentRequestConstant.ICCARD_NO, new String(request.getIcCardNo(), CHARSET));
+		map.put(RollbackPaymentRequestConstant.USER_CODE, new String(request.getUserCode(), CHARSET));
+		map.put(RollbackPaymentRequestConstant.USER_NAME, new String(request.getUserName(), CHARSET));
+		map.put(RollbackPaymentRequestConstant.BUY_TIME, ByteUtils.byteArray2Int(request.getBuyTime()));
+		map.put(RollbackPaymentRequestConstant.BUY_GAS, ByteUtils.byteArray2Long(request.getBuyGas()));
+		map.put(RollbackPaymentRequestConstant.ROLLBACK_MONEY, ByteUtils.byteArray2Long(request.getRollbackMoney()));
+		map.put(RollbackPaymentRequestConstant.BANK_SD, new String(request.getBankSD(), CHARSET));
+		map.put(RollbackPaymentRequestConstant.ROLLBACL_CAUSE, new String(request.getRollbackCause(), CHARSET));
+		map.put(RollbackPaymentRequestConstant.ENTRUST_UNIT_NO, new String(request.getEntrustUnitNo(), CHARSET));
+		map.put(RollbackPaymentRequestConstant.TERMINAL_BIZ_SN, ByteUtils.byteArray2Long(request.getTerminalBizSN()));
 	}
 
 	@Override
---
+++ src/main/java/cn/xuyingqi/netty/server/selfservice/protocol/datagram/constant/RollbackPaymentRequestConstant.java
@@ -0,0 +1,59 @@
+package cn.xuyingqi.netty.server.selfservice.protocol.datagram.constant;
+
+/**
+ * 冲正请求常量
+ * 
+ * @author XuYQ
+ *
+ */
+public final class RollbackPaymentRequestConstant {
+
+	/**
+	 * 卡类型
+	 */
+	public static final String ICCARD_TYPE = "ICCARD_TYPE";
+	/**
+	 * 控制器类型
+	 */
+	public static final String ICCARD_CONTROLLER = "ICCARD_CONTROLLER";
+	/**
+	 * 卡号
+	 */
+	public static final String ICCARD_NO = "ICCARD_NO";
+	/**
+	 * 用户号
+	 */
+	public static final String USER_CODE = "USER_CODE";
+	/**
+	 * 用户名
+	 */
+	public static final String USER_NAME = "USER_NAME";
+	/**
+	 * 购气次数
+	 */
+	public static final String BUY_TIME = "BUY_TIME";
+	/**
+	 * 购气量
+	 */
+	public static final String BUY_GAS = "BUY_GAS";
+	/**
+	 * 冲正金额
+	 */
+	public static final String ROLLBACK_MONEY = "ROLLBACK_MONEY";
+	/**
+	 * 银行业务流水号
+	 */
+	public static final String BANK_SD = "BANK_SD";
+	/**
+	 * 冲正原因
+	 */
+	public static final String ROLLBACL_CAUSE = "ROLLBACL_CAUSE";
+	/**
+	 * 委托单位编号
+	 */
+	public static final String ENTRUST_UNIT_NO = "ENTRUST_UNIT_NO";
+	/**
+	 * 终端业务流水号
+	 */
+	public static final String TERMINAL_BIZ_SN = "TERMINAL_BIZ_SN";
+}
